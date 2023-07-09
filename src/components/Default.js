272aa130146555fb0dcd3d98bc3b91b7f33fbaf1
sdrahmath <syedrahmath1410@gmail.com>
2023-07-09 12:30:37 +0530

    dynamic default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -6,12 +6,12 @@ function DefaultPage() {
         <h1>Welcome to Intellibot!</h1>
         
         <p>Start a conversation by clicking the <b>"+ New Conversation"</b> button.</p>
-        <p>To Generate image use "create image:" <br/>Ex:  Prompt(<b>create image: a red car with gold doors in 4k quality</b>)</p>
+        <p>To Generate image use Command <b>"create image:"</b> <br/></p>
       </div>
-      <div class="info-container">
-        <div class="info-plan">
-          <div class="info-title">Capabilities</div>
-          <ul class="info-features">
+      <div className="info-container">
+        <div className="info-plan">
+          <div className="info-title">Capabilities</div>
+          <ul className="info-features">
             <li>
             Remembers the user's past statements made across the conversation.
 
@@ -25,25 +25,28 @@ function DefaultPage() {
             </li>
           </ul>
         </div>
-        <div class="info-plan">
-          <div class="info-title">Examples</div>
-          <ul class="info-features">
-            <li> “What is Java?” ↵&#8195;&#8195;&#8195;&#8195; &#8195;&#8195;&#8195;&#8195; &#8195;&#8195;</li>
+        <div className="info-plan">
+          <div className="info-title">Examples</div>
+          <ul className="info-features">
+            <li> “What is Java?” ↵</li>
             <li>
               {" "}
               “Give me some inspiration for a serene painting"
-              ↵ &#8195;&#8195;&#8195;&#8195; &#8195;&#8195;
+              ↵ 
             </li>
             <li>
               {" "}
               "Provide a simple explanation of how fetch API works"
               ↵ 
             </li>
+            <li>
+            "create image: a red car with gold doors in 4k quality"↵ 
+            </li>
           </ul>
         </div>
-        <div class="info-plan">
-          <div class="info-title">Constraints</div>
-          <ul class="info-features">
+        <div className="info-plan">
+          <div className="info-title">Constraints</div>
+          <ul className="info-features">
             <li> Sometimes inaccurate information may be generated &#8195;&#8195;&#8195; &#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;&#8195;</li>
             <li> Occasionally, harmful or biased content may be generated &#8195;&#8195;&#8195;&#8195;</li>
             <li> Response chat limit : 100 words</li>
