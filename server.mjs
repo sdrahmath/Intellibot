7da446b4a474a19e98ec79df393a168beaebdf1c
Abdul Rahman Hussain Siddique <Abdulrahman.hussain02@gmail.com>
2023-07-09 21:56:24 +0530

    create image prompt
    added ":" to prompt but removed from sentence

--- server.mjs
+++ server.mjs
@@ -1,27 +1,23 @@
-import fetch from 'isomorphic-fetch';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.post('/completions', async (req, res) => {
+app.post("/completions", async (req, res) => {
   const { message } = req.body;
 
   // Check if the message includes the "create image" command
-  if (message.toLowerCase().includes('create image')) {
+  if (message.toLowerCase().includes("create image:")) {
     try {
       // Extract the remaining sentence after the command "create image"
-      const remainingSentence = message
-        .toLowerCase()
-        
-        
+      const remainingSentence = message.toLowerCase().excludes(":");
 
       // Call the DALL-E image generation API
       const imageUrl = await generateImage(remainingSentence);
@@ -29,8 +25,8 @@ app.post('/completions', async (req, res) => {
       // Return the image URL as the response
       res.send({ image: imageUrl });
     } catch (error) {
-      console.error('DALL-E image generation error:', error);
-      res.status(500).send({ error: 'Failed to generate image' });
+      console.error("DALL-E image generation error:", error);
+      res.status(500).send({ error: "Failed to generate image" });
     }
   } else {
     try {
@@ -40,57 +36,63 @@ app.post('/completions', async (req, res) => {
       // Return the completion as the response
       res.send({ completion });
     } catch (error) {
-      console.error('OpenAI chat completion error:', error);
-      res.status(500).send({ error: 'Failed to get chat completion' });
+      console.error("OpenAI chat completion error:", error);
+      res.status(500).send({ error: "Failed to get chat completion" });
     }
   }
 });
 
 async function generateImage(sentence) {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.API_KEY}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_KEY}`,
     },
     body: JSON.stringify({
       prompt: sentence,
       n: 1,
-      size: '512x512',
+      size: "512x512",
     }),
   };
 
-  const response = await fetch('https://api.openai.com/v1/images/generations', options);
+  const response = await fetch(
+    "https://api.openai.com/v1/images/generations",
+    options
+  );
   const data = await response.json();
 
   if (data.data && data.data.length > 0) {
     return data.data[0].url;
   } else {
-    throw new Error('Image generation failed');
+    throw new Error("Image generation failed");
   }
 }
 
 async function getChatCompletion(message) {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.API_KEY}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_KEY}`,
     },
     body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: message }],
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: message }],
       max_tokens: 50,
     }),
   };
 
-  const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+  const response = await fetch(
+    "https://api.openai.com/v1/chat/completions",
+    options
+  );
   const data = await response.json();
 
   if (data.choices && data.choices.length > 0) {
     return data.choices[0].message;
   } else {
-    throw new Error('Chat completion failed');
+    throw new Error("Chat completion failed");
   }
 }
 
