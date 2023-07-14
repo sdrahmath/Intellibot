514fda1936bd3fcb9440abc25cde9039321f5630
Abdul Rahman Hussain Siddique <Abdulrahman.hussain02@gmail.com>
2023-07-15 05:28:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
